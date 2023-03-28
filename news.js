6afc827fb2b78ba23b6fa317735dead6d9ddfa66
madjidzou <125971870+madjidzou@users.noreply.github.com>
2023-03-28 19:23:47 +0200

    Update news.js

--- news.js
+++ news.js
@@ -15,9 +15,6 @@ const fetchData = async (category,pageSize)=>{
 
 const add_breakingNews = (data)=>{
     breakingImg.innerHTML = `<img src=${data[0].urlToImage} alt="image">`
-    breakingNews_title.innerHTML = `<a href=${data[0].url} target="_blank"><h2>${data[0].title}</h2></a>`
-    breakingNews_desc.innerHTML = `${data[0].description}`
 }
-//fetchData('general',5).then(add_breakingNews)
-
+fetchData('general',5).then(add_breakingNews)
 
